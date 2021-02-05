eb067bac13d73932e4bf2979c6e96ffc2f1f0adc
sskunkworks <sskunkworks@gmail.com>
2021-02-05 07:35:34 -0800

    Display video

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print('py slam start')
+import cv2
+import numpy as np
 
+videoFile = 'video.mp4'
+cap = cv2.VideoCapture(videoFile)
 
-
+while cap.isOpened():
+  ret, frame = cap.read()
+  if ret == True:
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+  else:
+    break
+cap.release()
+cv2.destroyAllWindows()
