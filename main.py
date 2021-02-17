819399c5cb21d9f107ce16afa2319cbf60428618
sskunkworks <sskunkworks@gmail.com>
2021-02-17 07:28:32 -0800

    Draw Points and Line, but Need to check true circle,line?

--- main.py
+++ main.py
@@ -6,53 +6,50 @@ videoFile = '../video.mp4'
 q = collections.deque()
 qimg = collections.deque()
 cap = cv2.VideoCapture(videoFile)
-
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-#bf = cv2.BFMatcher()
 index = 1
 while cap.isOpened():
+  # Read current Image
   ret, frame = cap.read()
   if ret == True:
+    # Color to Gray Image
     gframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    orb = cv2.ORB_create()
-    # orb detect
-    # Train Index
-    kp2, des2 = orb.detectAndCompute(gframe,None)
-    qimg.append(gframe)
-    # Use goodfeature
-    #pts = cv2.goodFeaturesToTrack(gframe, 3000, 0.01, 10)
-    #kp = [cv2.KeyPoint(x=f[0][0],y=f[0][1],_size=20) for f in pts]
-    #kp2,des2 = orb.compute(frame, kp)
-    q.append([kp2,des2])
+
+    # Need 2 frames more
     if index >= 2:
       # Query Index
-      kp1, des1 = q.pop() 
-      img1 = qimg.pop()
-      matches = bf.match(des1, des2)
-      #matches = bf.knnMatch(des1,des2,k=2)
-      matches = sorted(matches, key=lambda x:x.distance)
-      #img3 = cv2.drawMatches(img1, kp1, gframe, kp2, matches[:10],None,flags=2) 
-      #cv2.imshow('frame',img3)
-      for idx in range(0,len(matches)):
-        trainIdx =  matches[idx].trainIdx
-        queryIdx = matches[idx].queryIdx 
-        train_x = kp1[trainIdx].pt[0]
-        train_y = kp1[trainIdx].pt[1]
-        query_x = kp2[queryIdx].pt[0]
-        query_y = kp2[queryIdx].pt[1]
-        #print(train_x, query_x, train_y, query_y)
-        cv2.circle(frame, (int(train_x),int(train_y)), color=(0,255,0), radius=3)
-        cv2.circle(frame, (int(query_x),int(query_y)), color=(0,0,255), radius=3)
-        cv2.line(frame, (int(train_x),int(train_y)), (int(query_x)+5,int(query_y)),color=(255,0,0))
-      cv2.imshow('frame', frame)
-    points = []
-    #for p in kp:
-    #  points.append([p.pt[0], p.pt[1]])
-    #  #print(p.pt[0], p.pt[1])
-    #  cv2.circle(frame, (int(p.pt[0]),int(p.pt[1])), color=(0,255,0), radius=3)
-    #cv2.circle(frame, (u1,v1), color=(0,255,0), radius=3)
-    #img2 = cv2.drawKeypoints(frame,kp,outImage=None,color=(0,255,0),flags=0)
-    #cv2.imshow('frame', frame)
+      img_ref = qimg.pop()
+      img_cur = gframe
+
+      orb = cv2.ORB_create()
+      pts = cv2.goodFeaturesToTrack(img_cur, 3000, qualityLevel=0.01, minDistance=3)
+      kps = [cv2.KeyPoint(x=f[0][0],y=f[0][1], _size=20) for f in pts]
+      kps_cur,des_cur = orb.compute(gframe,kps)
+      # matching 
+      # Need 3 datas more
+      if index >= 3:
+        kps_ref, des_ref = q.pop() 
+        pts_cur = pts
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+        matches = bf.knnMatch(des_ref, des_cur, k=2) # query, train
+        p_cur,p_ref = [],[]
+        for m,n in matches:
+          #print(m.distance,n.distance)
+          if m.distance < n.distance*0.3:
+            query_idx = m.queryIdx
+            train_idx = m.trainIdx
+            train_x, train_y = kps_cur[train_idx].pt[0], kps_cur[train_idx].pt[1]
+            query_x, query_y = kps_ref[query_idx].pt[0], kps_ref[query_idx].pt[1]
+            #print(train_x,train_y)
+            #cv2.circle(frame, (int(train_x),int(train_y)), color=(0,255,0), radius=3) # green, past feature
+            cv2.circle(frame, (int(query_x),int(query_y)), color=(0,0,255), radius=3) # red, current feature
+            cv2.line(frame, (int(train_x),int(train_y)), (int(query_x),int(query_y)),color=(255,0,0)) # green to red line
+         
+      # Past Keypoints, Descriptors
+      q.append([kps_cur,des_cur])
+      cv2.imshow('frame',frame)
+    
+    qimg.append(gframe)
+ 
     if cv2.waitKey(25) & 0xFF == ord('q'):
       break
     index += 1
